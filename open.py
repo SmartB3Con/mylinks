ebf172596898be3d431ae5b52415c1a80d3d02f5
SmartB3Con <74354484+SmartB3Con@users.noreply.github.com>
2021-01-15 10:05:40 +0530

    Add files via upload

---
+++ open.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+e=input("Would you like to check out my links?")
+if e.lower() == 'yes' or 'Yes':
+    print("Check your browser now.") 
+    time.sleep(1)
+    webbrowser.open('https://www.youtube.com/channel/UCvZzAojE8DFEemoIJI_40jw')
+    webbrowser.open("https://website.baconhead.repl.co/")
+    webbrowser.open("https://discord.com/oauth2/authorize?client_id=763254971234779157&scope=bot&permissions=8)")
+    webbrowser.open("https://discord.gg/5raGMZE")
+    
